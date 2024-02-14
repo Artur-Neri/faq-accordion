1a2421a670b70e08cc4d9a711bb4eb5fd851ba53
Artur-Neri <arturnerictt@gmail.com>
2024-02-14 17:12:06 -0300

    logica de troca de imagens de expasão

--- app/main.js
+++ app/main.js
@@ -4,8 +4,13 @@ itens.forEach(element => {
     element.addEventListener('click', abreItem);
 });
 
-
 function abreItem() {
     const answer = this.children[1]
     answer.classList.toggle('hide');
+
+    if (answer.classList.contains('hide')) {
+        this.children[0].children[1].src = './assets/images/icon-plus.svg';
+    } else {
+        this.children[0].children[1].src = './assets/images/icon-minus.svg'
+    }
 }
